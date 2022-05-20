3d19acdf7decbd7948154f03bda2bafd49f55b75
Mariam <maremrady2017@gmail.com>
2022-05-21 00:48:53 +0200

    Initial commit

---
+++ app/src/main/java/com/route/islami_c35_sat/ui/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.route.islami_c35_sat.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.route.islami_c35_sat.ui.home.HomeActivity
+import com.route.islami_c35_sat.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                startHomeActivity();
+            },2000);
+    }
+    fun startHomeActivity(){
+        val intent = Intent(this, HomeActivity::class.java);
+        startActivity(intent);
+        finish()
+
+    }
+}
